f498afb033542c170b5f3e7beb0b7f5cd23e3cfd
willianchu <willianchu@hotmail.com>
2022-09-28 20:57:59 -0700

    article list page

--- my-codeblog/src/pages/ArticleListPage.js
+++ my-codeblog/src/pages/ArticleListPage.js
@@ -1,6 +1,16 @@
+import articles from './article-content';
+
 const ArticleListPage = () => {
   return(
-    <h1> Article List Page </h1>
+    <>
+      <h1> Articles </h1>
+      {articles.map(article => 
+        <div>
+          <h3>{article.title}</h3>
+          <p>{article.content[0].substring(0, 150)}...</p>  
+        </div>
+        )}
+    </>
   );
 }
 
