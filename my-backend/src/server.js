8726a02d653c50adb40ba4fc5f061d37eadab985
willianchu <willianchu@hotmail.com>
2022-10-01 17:36:03 -0700

    set get post test to the postman

--- my-backend/src/server.js
+++ my-backend/src/server.js
@@ -1,10 +1,19 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/hello', (req, res)=>{
-  res.send('Hello!');
+app.post('/hello', (req, res)=>{
+  console.log(req.body);
+  const user = req.body;
+  res.send(`Hello! ${user.name}`);
 });
+
+app.get('/hello/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello ${name}!!`);
+});
+
 
 app.listen(8000, ()=> {
   console.log("Server is listening on port 8000");
